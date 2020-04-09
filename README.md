a09d211bb6d9150432377c14a826efa71c1d219a
eric m <ericmuckley@gmail.com>
2020-04-08 20:45:10 -0400

    updated module

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Microscopy analysis
-Analysis of microscopy images in **.dm3** format.
+
+This notebook and associated methods perform analysis of microscopy images in **.dm3** format by implementing a sliding window which rasters over each image and extracts satstistical information about crystallite phases and orientation. 
+
+
+### Description of files
+
+* **read_stack.ipynb**: Jupyter Notebook which performs analysis of microscopy images
+* **microscopy_methods.py**: python module which contains functions for opening image files and implementing sliding analysis window
+
--- microscopy_methods.py
+++ microscopy_methods.py
@@ -20,7 +20,7 @@ def plot_setup(labels=['X', 'Y'], fsize=18, title=''):
     plt.ylabel(str(labels[1]), fontsize=fsize)
     plt.title(title, fontsize=fsize)
     fig = plt.gcf()
-    fig.set_size_inches(6, 6)
+    fig.set_size_inches(8, 8)
 
 def read_stack(filepath):
     """Read dm3 stack file and get image information."""
@@ -36,3 +36,30 @@ def norm_image(img):
     img = img - img.min()
     img = img / img.max()
     return img
+
+def get_window_slices(img, samples, oversamples):
+    """Get index slices which describe bounds of a sliding
+    window that rasters accross a 2D image.
+    Two sliding windows are created:
+    1. a sampling window, with 'samples' number of pixels
+    2. an oversmaplign window, with 'oversamples' number of pixels
+    Returns a dictionary of the index slices for both sliding windows."""
+    # get list of coordinates at which to anchor each sampling window
+    window_anchors_x = np.arange(0, len(img[0]), samples)
+    window_anchors_y = np.arange(0, len(img), samples)
+    window_coords = np.array(np.meshgrid(
+        window_anchors_x, window_anchors_y)).T.reshape(-1,2)
+    # create lists for sample and oversample window index slices
+    s_slices, os_slices = [], []
+    # loop over each set of coordinates for the window anchor
+    for x0, y0 in window_coords:
+        # find indices of oversampled window
+        over_x0 = np.clip(x0-oversamples, 0, len(img[0]))
+        over_x1 = np.clip(x0+oversamples, 0, len(img[0]))
+        over_y0 = np.clip(y0-oversamples, 0, len(img))
+        over_y1 = np.clip(y0+oversamples, 0, len(img))
+        os_slice = np.s_[over_y0:over_y1, over_x0:over_x1]
+        s_slice = np.s_[y0:y0+samples, x0:x0+samples]
+        s_slices.append(s_slice)
+        os_slices.append(os_slice)
+    return {'s': s_slices, 'os': os_slices}
