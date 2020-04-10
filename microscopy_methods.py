dba9f3f6dafe9d1c064f1988bd441d26075068a8
eric m <ericmuckley@gmail.com>
2020-04-10 14:51:38 -0400

    added kmeans mapping

--- microscopy_methods.py
+++ microscopy_methods.py
@@ -14,10 +14,11 @@ author: ericmuckley@gmail.com
 import os
 import cv2
 import sklearn
-from sklearn import cluster
 import skimage
 import numpy as np
 from ncempy.io import dm
+import scipy.fftpack as fp
+from sklearn import cluster
 import matplotlib.pyplot as plt
 
 # change matplotlib settings to make plots look nicer
@@ -30,31 +31,74 @@ plt.rcParams['ytick.minor.width'] = 3
 plt.rcParams['ytick.major.width'] = 3
 
 
-def get_domains(img, n_clusters=2, contour_level=0.5, min_area=1, max_area=100000):
-    """Get crystallite domain information extracted from an image.
-    First, 'n_clusters' number of domains are found by K-means
-    clustering. Then contours of k-means cluster map are generted,
-    which serve as domain boundaries between clusters. Finally, cluster
-    areas are calculated. Returns a dictionary of countour lines which
-    define each cluster domain and area of each domain."""
+def plot_domains(domains, plot_contours=True, plot_areas=True, plot_domain_markers=True):
+    """Plot domain boundaries and areas on an image.
+    Uses 'domains' dictionary which holds information
+    about domain contours and areas."""
+    # loop over each domain
+    for i in range(len(domains['areas'])):
+        if plot_contours:
+            # plot contour line
+            plt.plot(domains['contours'][i][:,0],
+                     domains['contours'][i][:,1],
+                     linewidth=1.5,
+                     c='r')
+        if plot_areas:
+            # plot areas on domain map
+            plt.text(domains['mean_x'][i],
+                     domains['mean_y'][i],
+                     str(round(domains['areas'][i])))
+        if plot_domain_markers:
+            # plot marker in the center of each domain
+            plt.scatter(domains['mean_x'][i],
+                        domains['mean_y'][i],
+                        marker='x',
+                        c='r',
+                        s=50)
+
+def print_domain_info(domains):
+    """Print statistics related to calculated domains."""
+    print('Detected {} domains with mean area of {} pixels'.format(
+        len(domains['areas']), round(np.mean(domains['areas']),1)))
+    print('Total image area (pixels): {}'.format(domains['tot_area']))
+    print('Total domain area (pixels): {}'.format(int(domains['tot_domain_area'])))
+    print('Total domain area (%): {}'.format(round(domains['domain_percent'], 1)))
+
+def get_kmeans_map(img, n_clusters=2):
+    """Perform K-means clustering algorithm to find similarly-colored
+    regions in an image. Number fo regions is equal to 'n_clusters'."""
     # perform k-means clustering and detect contours of kmeans map
     kmeans = cluster.KMeans(n_clusters=n_clusters)
     kmeans.fit(img.reshape((-1, 1)))
     # create 2D map of k-means clustering results
     kmeans_map = norm_image(kmeans.labels_.reshape(img.shape))
+    return kmeans_map
+
+def get_domains(img, contour_level=0.5, min_area=1, max_area=100000):
+    """Get crystallite domain information extracted from an image.
+    Use a highly contrasted image, such as one output from the
+    'get_kmeans_map' function. areas are calculated. Returns a dictionary of countour lines which
+    define each cluster domain and area of each domain."""
     # extract contour line ordered pairs from the map
-    contours = skimage.measure.find_contours(kmeans_map, contour_level)
-    # clasulate area of inside each contour line
+    contours = skimage.measure.find_contours(img, contour_level)
+    # calculate area of inside each contour line
     areas = [cv2.contourArea(cv2.UMat(
         np.expand_dims(c.astype(np.float32), 1))) for c in contours]
     # create zipped list of areas and contour lines below area threshold
     zipped = [i for i in zip(areas, contours) if min_area <= i[0] <= max_area]
     # sort from large area to small area domains
     zipped.sort(key=lambda x: x[0], reverse=True)
+    # get domain statistics
+    tot_area = img.shape[0]*img.shape[1]
+    tot_domain_area = np.sum([z[0] for z in zipped])
+    domain_percent = 100 * tot_domain_area / tot_area
     # create dictionary to hold results
     domains = {
-        'area': [z[0] for z in zipped],
-        'contour': [np.flip(z[1]) for z in zipped],
+        'tot_area': int(tot_area),
+        'tot_domain_area': int(tot_domain_area),
+        'domain_percent': domain_percent,
+        'areas': [z[0] for z in zipped],
+        'contours': [np.flip(z[1]) for z in zipped],
         'mean_x': [np.mean(z[1][:, 1]) for z in zipped],
         'mean_y': [np.mean(z[1][:, 0]) for z in zipped]}
     return domains
@@ -103,7 +147,7 @@ def read_stack(filepath):
 def norm_image(img):
     """Normalize an image so its min and max stretch from 0 to 1."""
     median, std = np.median(img), np.std(img)
-    low_clip, high_clip = median-7*std, median+7*std
+    low_clip, high_clip = median-5*std, median+5*std
     img = np.clip(img, low_clip, high_clip)
     img = img - img.min()
     img = img / img.max()
@@ -141,8 +185,8 @@ def get_gradient_info(img):
     """Get magnitude and phase of gradient of 2D image."""
     grad_x, grad_y = np.abs(np.gradient(img))
     grad_mag = np.mean(np.hypot(grad_x, grad_y))
-    grad_phase = np.mean(np.arctan(grad_y/grad_x))
-    return grad_mag, grad_phase
+    #grad_phase = np.mean(np.arctan(grad_y/grad_x))
+    return grad_mag
     
 def map_image(img, slices):
     """Create maps of an image using a sliding window."""
@@ -159,7 +203,7 @@ def map_image(img, slices):
         # get oversampled window
         osw = img[slices['os'][w]]
         # get statistics of oversampled window
-        gradient_mag, gradient_phase = get_gradient_info(osw)
+        gradient_mag = get_gradient_info(osw)
         # get slice of sampled window
         sw = slices['s'][w]
         # save statistics of sampled image window
